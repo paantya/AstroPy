041f97730550fb20650314338a2bc75636b2ba9c
paantya <paantya@gmail.com>
2016-10-20 02:47:59 +0300

    work function

--- AstroPy.py
+++ AstroPy.py
@@ -1,12 +1,12 @@
-import cos, sin from numpy
+import numpy as np
 
 def elerToDecartXYZ(a, e, i, om, w, m):
 	u = w
 	E = m
-	r = a * (1 - e * cos(E))
-	x_or = r * cos(u)
-	y_or = r * sin(u)
-	z = y_or * sin(i)
-	x = x_or * cos(om) - y_or * cos(i) * sin(om)
-	y = x_or * sin(om) + y_or * cos(i) * cos(om)
+	r = a * (1 - e * np.cos(E))
+	x_or = r * np.cos(u)
+	y_or = r * np.sin(u)
+	z = y_or * np.sin(i)
+	x = x_or * np.cos(om) - y_or * np.cos(i) * np.sin(om)
+	y = x_or * np.sin(om) + y_or * np.cos(i) * np.cos(om)
 	return [x,y,z]
